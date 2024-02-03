e18c7aa7860ff17f976fec42638597e6adb1f4cc
MadinaMa <149488374+MadinaMa@users.noreply.github.com>
2024-02-03 13:39:05 +0100

    Updated

--- src/Icons.js
+++ src/Icons.js
@@ -9,19 +9,19 @@ import wup from './IconsImages/wup.png';
 function Icons() {
     return ( <div>
         <div className="socials">
-        <a href='https://www.instagram.com/myo_orthomagic?igsh=aXFyNmloaDYyZmk4' target="_blank"> <img src={insta} alt='insta' width="40px" height="40px"/></a>
+        <a href='https://www.instagram.com/myo_orthomagic?igsh=aXFyNmloaDYyZmk4' target="_blank" rel="noreferrer"> <img src={insta} alt='insta' width="40px" height="40px"/></a>
         </div>
 
        <div className='socials'>
-       <a href="https://t.me/olik_tigra13" target="_blank"> <img src={telega} alt='telega' width="40px" height="40px"/></a>
+       <a href="https://t.me/olik_tigra13" target="_blank" rel="noreferrer"> <img src={telega} alt='telega' width="40px" height="40px"/></a>
         </div>
 
         <div className='socials'>
-        <a href="tel:+7 (965) 078-72-13" target="_blank" rel=''> <img src={wup} alt='whatsup' width="40px" height="40px"/></a>
+        <a href="tel:+7 (965) 078-72-13" target="_blank" rel="noreferrer"> <img src={wup} alt='whatsup' width="40px" height="40px"/></a>
         </div>
 
         <div className='socials'>
-        <a href='https://m.vk.com/clubortomagic' target="_blank"> <img src={vk} alt='vk' width="40px" height="40px"/></a>
+        <a href='https://m.vk.com/clubortomagic' target="_blank" rel="noreferrer"> <img src={vk} alt='vk' width="40px" height="40px"/></a>
         </div>
 
         </div>
